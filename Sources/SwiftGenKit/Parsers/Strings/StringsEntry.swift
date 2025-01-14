4d4161fe62acca6058b7e29e9be691921c645b32
Mohammed Ahmad <>
2025-01-14 15:59:12 +0000

    Adds param extraction and tests for strings files

--- Sources/SwiftGenKit/Parsers/Strings/StringsEntry.swift
+++ Sources/SwiftGenKit/Parsers/Strings/StringsEntry.swift
@@ -8,31 +8,47 @@ import Foundation
 import PathKit
 
 extension Strings {
-  struct Entry {
-    var comment: String?
-    let key: String
-    let translation: String
-    let types: [PlaceholderType]
-    let keyStructure: [String]
+    struct Entry {
+        var comment: String?
+        let key: String
+        let translation: String
+        let types: [PlaceholderType]
+        let parameters: [Parameter]
+        let keyStructure: [String]
+        
+        init(
+            key: String,
+            translation: String,
+            types: [PlaceholderType],
+            parameters: [Parameter],
+            keyStructureSeparator: String
+        ) {
+            self.key = key
+            self.translation = translation
+            self.types = types
+            self.parameters = parameters
+            self.keyStructure = Self.split(key: key, separator: keyStructureSeparator)
+        }
+        
+        init(key: String, translation: String, keyStructureSeparator: String) throws {
+            let types = try PlaceholderType.placeholderTypes(fromFormat: translation)
+            let parameters = Parameter.extractParameterNames(from: translation, type: .object)
 
-    init(key: String, translation: String, types: [PlaceholderType], keyStructureSeparator: String) {
-      self.key = key
-      self.translation = translation
-      self.types = types
-      self.keyStructure = Self.split(key: key, separator: keyStructureSeparator)
+            self.init(
+                key: key,
+                translation: translation,
+                types: types,
+                parameters: parameters,
+                keyStructureSeparator: keyStructureSeparator
+            )
+        }
+        
+        // MARK: - Structured keys
+        
+        private static func split(key: String, separator: String) -> [String] {
+            key
+                .components(separatedBy: separator)
+                .filter { !$0.isEmpty }
+        }
     }
-
-    init(key: String, translation: String, keyStructureSeparator: String) throws {
-      let types = try PlaceholderType.placeholderTypes(fromFormat: translation)
-      self.init(key: key, translation: translation, types: types, keyStructureSeparator: keyStructureSeparator)
-    }
-
-    // MARK: - Structured keys
-
-    private static func split(key: String, separator: String) -> [String] {
-      key
-        .components(separatedBy: separator)
-        .filter { !$0.isEmpty }
-    }
-  }
 }
---
+++ Tests/SwiftGenKitTests/StringsFileParserTests.swift
@@ -0,0 +1,80 @@
+@testable import SwiftGenKit
+import TestUtils
+import XCTest
+
+final class StringsFileParserTests: XCTestCase {
+    private var parser: Strings.StringsFileParser!
+    private var options: ParserOptionValues!
+    
+    override func setUp() {
+        super.setUp()
+        let options = try! ParserOptionValues(options: [:], available: Strings.Parser.allOptions)
+        parser = Strings.StringsFileParser(options: options)
+    }
+    
+    override func tearDown() {
+        parser = nil
+        options = nil
+        super.tearDown()
+    }
+    
+    func test_parseFile_readsKeys() throws {
+        // Given
+        let filePath = Fixtures.resource(for: "LocNamedParameters.strings", sub: .strings)
+        
+        // When
+        let stringsEntry = try parser.parseFile(at: filePath)
+        
+        // Then
+        let keys = stringsEntry.map { $0.key }
+        XCTAssertEqual(
+            keys.sorted(),
+            ["no_parameter", "one_parameter", "two_parameters"]
+        )
+    }
+    
+    func test_parseFile_readsTranslations() throws {
+        // Given
+        let filePath = Fixtures.resource(for: "LocNamedParameters.strings", sub: .strings)
+        
+        // When
+        let stringsEntry = try parser.parseFile(at: filePath)
+        
+        // Then
+        let translations = stringsEntry.map { $0.translation }
+        XCTAssertEqual(
+            translations.sorted(),
+            [
+                "Hello, {{name}}!",
+                "This string has no named parameters",
+                "Welcome, {{name}}! You have {{messageCount}} unread messages."
+            ]
+        )
+    }
+    
+    func test_parseFile_readsParameters() throws {
+        // Given
+        let filePath = Fixtures.resource(for: "LocNamedParameters.strings", sub: .strings)
+        
+        // When
+        let stringsEntry = try parser.parseFile(at: filePath)
+        
+        // Then
+        let parameters = stringsEntry
+            .map { $0.parameters }
+            .filter { !$0.isEmpty }
+            .sorted(by: { $0.count < $1.count })
+        
+        XCTAssertEqual(
+            parameters[0],
+            [.init(name: "name", type: .object)]
+        )
+        XCTAssertEqual(
+            parameters[1],
+            [
+                .init(name: "name", type: .object),
+                .init(name: "messageCount", type: .object)
+            ]
+        )
+    }
+}
