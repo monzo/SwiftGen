f7856ebfdf1123fa1bed7c6b33f490a6e5794b44
Mohammed Ahmad <>
2025-01-23 11:29:52 +0000

    Bumps internal versions to 6.6.4 to align with the release

--- Sources/SwiftGen/Version.swift
+++ Sources/SwiftGen/Version.swift
@@ -5,8 +5,8 @@
 //
 
 enum Version {
-  static let swiftgen = "6.6.3"
-  static let swiftGenKit = "6.6.3"
+  static let swiftgen = "6.6.4"
+  static let swiftGenKit = "6.6.4"
   static let stencil = "0.15.1"
   static let stencilSwiftKit = "2.10.1"
 }
