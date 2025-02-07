69643b7d06d8e259197a9b7397d8d1577d3d9e75
Mohammed Ahmad <>
2025-02-07 16:54:21 +0000

    Removes conversion from snake to camel case in `Parameters` and fixes indentation to be 2 space to make it easier to merge back in upstream

--- Sources/SwiftGenKit/Parsers/Strings/Parameter.swift
+++ Sources/SwiftGenKit/Parsers/Strings/Parameter.swift
@@ -3,43 +3,43 @@ import Foundation
 // MARK: - Parameter Model
 
 extension Strings {
-    public struct Parameter: Equatable {
-        public let name: String
-        public let type: PlaceholderType
-    }
+  public struct Parameter: Equatable {
+    public let name: String
+    public let type: PlaceholderType
+  }
 }
 
 // MARK: - Extract Parameter Names
 
 extension Strings.Parameter {
-    /// Extracts parameter names from a given string
-    ///
-    /// - Parameters:
-    ///   - string: The input string containing potential parameters
-    ///   - type: The placeholder type used to assign a type to each extracted parameter
-    /// - Returns: An array of `Parameter` objects representing the extracted parameters
-    ///
-    /// Example: "Welcome, {{displayName}}" --> [Parameter(name: "displayName", type: .string)]
-    static func extractParameterNames(
-        from string: String,
-        type: Strings.PlaceholderType
-    ) -> [Strings.Parameter] {
-        let results = namedParameterRegEx.matches(
-            in: string,
-            options: [],
-            range: NSRange(location: 0, length: string.utf16.count)
-        )
-        
-        let parameterNames = results.compactMap { result -> String? in
-            /// `result.range(at: 1)` extracts the first capture group (i.e. the parameter name)
-            guard let range = Range(result.range(at: 1), in: string) else {
-                return nil
-            }
-            return String(string[range])
-        }
-        
-        return parameterNames.map { .init(name: $0.snakeToCamelCase, type: type) }
+  /// Extracts parameter names from a given string
+  ///
+  /// - Parameters:
+  ///   - string: The input string containing potential parameters
+  ///   - type: The placeholder type used to assign a type to each extracted parameter
+  /// - Returns: An array of `Parameter` objects representing the extracted parameters
+  ///
+  /// Example: "Welcome, {{displayName}}" --> [Parameter(name: "displayName", type: .string)]
+  static func extractParameterNames(
+    from string: String,
+    type: Strings.PlaceholderType
+  ) -> [Strings.Parameter] {
+    let results = namedParameterRegEx.matches(
+      in: string,
+      options: [],
+      range: NSRange(location: 0, length: string.utf16.count)
+    )
+    
+    let parameterNames = results.compactMap { result -> String? in
+      /// `result.range(at: 1)` extracts the first capture group (i.e. the parameter name)
+      guard let range = Range(result.range(at: 1), in: string) else {
+        return nil
+      }
+      return String(string[range])
     }
+    
+    return parameterNames.map { .init(name: $0, type: type) }
+  }
 }
 
 // MARK: - Private Helpers
@@ -47,45 +47,19 @@ extension Strings.Parameter {
 // MARK: Parameter Regex
 
 private extension Strings.Parameter {
-    /// This regular expression is used to match named parameters embedded within double curly braces in a string.
-    ///
-    /// - Note: The pattern will match sequences like `{{parameterName}}`, where `parameterName` consists of
-    /// alphanumeric characters and underscores, and must start with a lowercase letter. The expression allows
-    /// for optional whitespace both inside and outside the braces.
-    static let namedParameterRegEx: NSRegularExpression = {
-        do {
-            return try NSRegularExpression(
-                pattern: #"\{\{\s*([a-z][a-z0-9_]*)\s*\}\}"#,
-                options: []
-            )
-        } catch {
-            fatalError("Error building the regular expression used to match the named parameter format")
-        }
-    }()
-}
-
-// MARK: String Formatting
-
-private extension String {
-    /// Converts a `snake_case` string to `camelCase`.
-    ///
-    /// - Note: Handles edge cases such as leading, trailing, and multiple underscores by ignoring them
-    /// and not including empty segments in the output. If the input is an empty string or consists only
-    /// of underscores, the computed property will return an empty string.
-    var snakeToCamelCase: String {
-        guard !self.isEmpty else { return "" }
-
-        let components = self.split(separator: "_")
-        
-        guard !components.isEmpty else { return "" }
-        
-        return components.enumerated().map { index, element in
-            let lowercased = element.lowercased()
-            return index == 0 ? lowercased : lowercased.capitalizingFirstLetter
-        }.joined()
+  /// This regular expression is used to match named parameters embedded within double curly braces in a string.
+  ///
+  /// - Note: The pattern will match sequences like `{{parameterName}}`, where `parameterName` consists of
+  /// alphanumeric characters and underscores, and must start with a lowercase letter. The expression allows
+  /// for optional whitespace both inside and outside the braces.
+  static let namedParameterRegEx: NSRegularExpression = {
+    do {
+      return try NSRegularExpression(
+        pattern: #"\{\{\s*([a-z][a-z0-9_]*)\s*\}\}"#,
+        options: []
+      )
+    } catch {
+      fatalError("Error building the regular expression used to match the named parameter format")
     }
-    
-    var capitalizingFirstLetter: String {
-        prefix(1).capitalized + dropFirst()
-    }
+  }()
 }
--- Tests/SwiftGenKitTests/StringsFileParserTests.swift
+++ Tests/SwiftGenKitTests/StringsFileParserTests.swift
@@ -3,91 +3,91 @@ import TestUtils
 import XCTest
 
 final class StringsFileParserTests: XCTestCase {
-    private var parser: Strings.StringsFileParser!
-    private var options: ParserOptionValues!
+  private var parser: Strings.StringsFileParser!
+  private var options: ParserOptionValues!
+  
+  override func setUp() {
+    super.setUp()
+    let options = try! ParserOptionValues(options: [:], available: Strings.Parser.allOptions)
+    parser = Strings.StringsFileParser(options: options)
+  }
+  
+  override func tearDown() {
+    parser = nil
+    options = nil
+    super.tearDown()
+  }
+  
+  func test_parseFile_readsKeys() throws {
+    // Given
+    let filePath = Fixtures.resource(for: "LocNamedParameters.strings", sub: .strings)
     
-    override func setUp() {
-        super.setUp()
-        let options = try! ParserOptionValues(options: [:], available: Strings.Parser.allOptions)
-        parser = Strings.StringsFileParser(options: options)
-    }
+    // When
+    let stringsEntry = try parser.parseFile(at: filePath)
     
-    override func tearDown() {
-        parser = nil
-        options = nil
-        super.tearDown()
-    }
+    // Then
+    let keys = stringsEntry.map { $0.key }
+    XCTAssertEqual(
+      keys.sorted(),
+      ["no.parameter", "one.parameter", "two.parameters"]
+    )
+  }
+  
+  func test_parseFile_readsTranslations() throws {
+    // Given
+    let filePath = Fixtures.resource(for: "LocNamedParameters.strings", sub: .strings)
     
-    func test_parseFile_readsKeys() throws {
-        // Given
-        let filePath = Fixtures.resource(for: "LocNamedParameters.strings", sub: .strings)
-        
-        // When
-        let stringsEntry = try parser.parseFile(at: filePath)
-        
-        // Then
-        let keys = stringsEntry.map { $0.key }
-        XCTAssertEqual(
-            keys.sorted(),
-            ["no.parameter", "one.parameter", "two.parameters"]
-        )
-    }
+    // When
+    let stringsEntry = try parser.parseFile(at: filePath)
     
-    func test_parseFile_readsTranslations() throws {
-        // Given
-        let filePath = Fixtures.resource(for: "LocNamedParameters.strings", sub: .strings)
-        
-        // When
-        let stringsEntry = try parser.parseFile(at: filePath)
-        
-        // Then
-        let translations = stringsEntry.map { $0.translation }
-        XCTAssertEqual(
-            translations.sorted(),
-            [
-                "Hello, {{name}}!",
-                "This string has no named parameters",
-                "Welcome, {{name}}! You have {{message_count}} unread messages."
-            ]
-        )
-    }
+    // Then
+    let translations = stringsEntry.map { $0.translation }
+    XCTAssertEqual(
+      translations.sorted(),
+      [
+        "Hello, {{name}}!",
+        "This string has no named parameters",
+        "Welcome, {{name}}! You have {{message_count}} unread messages."
+      ]
+    )
+  }
+  
+  func test_parseFile_readsParameters() throws {
+    // Given
+    let filePath = Fixtures.resource(for: "LocNamedParameters.strings", sub: .strings)
     
-    func test_parseFile_readsParameters() throws {
-        // Given
-        let filePath = Fixtures.resource(for: "LocNamedParameters.strings", sub: .strings)
-        
-        // When
-        let stringsEntry = try parser.parseFile(at: filePath)
-        
-        // Then
-        let parameters = stringsEntry
-            .map { $0.parameters }
-            .sorted(by: { $0.count < $1.count })
-        
-        let firstEntryParameters = parameters[0]
-        XCTAssertEqual(firstEntryParameters.count, 0)
-        XCTAssertEqual(
-            firstEntryParameters,
-            []
-        )
-        
-        let secondEntryParameters = parameters[1]
-        XCTAssertEqual(secondEntryParameters.count, 1)
-        XCTAssertEqual(
-            secondEntryParameters,
-            [
-                .init(name: "name", type: .object)
-            ]
-        )
-        
-        let thirdEntryParameters = parameters[2]
-        XCTAssertEqual(thirdEntryParameters.count, 2)
-        XCTAssertEqual(
-            thirdEntryParameters,
-            [
-                .init(name: "name", type: .object),
-                .init(name: "messageCount", type: .object)
-            ]
-        )
-    }
+    // When
+    let stringsEntry = try parser.parseFile(at: filePath)
+    
+    // Then
+    let parameters = stringsEntry
+      .map { $0.parameters }
+      .sorted(by: { $0.count < $1.count })
+    
+    let firstEntryParameters = parameters[0]
+    XCTAssertEqual(firstEntryParameters.count, 0)
+    XCTAssertEqual(
+      firstEntryParameters,
+      []
+    )
+    
+    let secondEntryParameters = parameters[1]
+    XCTAssertEqual(secondEntryParameters.count, 1)
+    XCTAssertEqual(
+      secondEntryParameters,
+      [
+        .init(name: "name", type: .object)
+      ]
+    )
+    
+    let thirdEntryParameters = parameters[2]
+    XCTAssertEqual(thirdEntryParameters.count, 2)
+    XCTAssertEqual(
+      thirdEntryParameters,
+      [
+        .init(name: "name", type: .object),
+        .init(name: "message_count", type: .object)
+      ]
+    )
+  }
 }
